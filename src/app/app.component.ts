affc6e31d6614eb2c30be7ea2a8375b3e5bc629a
stoykoj <stoykojulia@gmail.com>
2019-03-23 11:41:30 -0500

    Add HttpClient to communicate with Rails

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
+// src/app/app.component.ts
+ 
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HttpClient]
 })
 export class AppComponent {
-  title = 'Y';
+  title = 'Job Finder';
+  employees;
+ 
+  constructor(private http: HttpClient) {
+    this.http.get('http://localhost:3000/employees.json')
+      .subscribe(res => this.employees = res);
+  }
 }
